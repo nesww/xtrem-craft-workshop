1a9a9b36fd863298fa01241abcd04ac13af8f50f
Labbé Clément <clement.labbe33@gmail.com>
2026-03-17 09:17:53 +0100

    chore(java) refactoring money

--- java/src/main/java/money_problem/domain/Money.java
+++ java/src/main/java/money_problem/domain/Money.java
@@ -1,13 +1,18 @@
 package money_problem.domain;
 
 public record Money (double amount, Currency currency) {
-    public Money add(double amount) {
-        return new Money(amount + this.amount, this.currency);
+    public Money add(Money other) throws Exception{
+        if (other.currency != this.currency){
+            throw new Exception("Feur ! ");
+        }
+        return new Money(other.amount + this.amount, this.currency);
     }
-    public Money times(double amount) {
-        return new Money(amount * this.amount, this.currency);
+    
+    public Money times(int times) {
+        return new Money(times* this.amount, this.currency);
     }
-    public Money divide(double amount) {
-        return new Money(this.amount / amount, this.currency);
+    
+    public Money divide(int by){
+        return new Money(this.amount / by, this.currency);
     }
 }
--- java/src/test/java/money_problem/domain/MoneyTest.java
+++ java/src/test/java/money_problem/domain/MoneyTest.java
@@ -1,5 +1,9 @@
 package money_problem.domain;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import static money_problem.domain.Currency.*;
@@ -8,8 +12,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MoneyTest {
     @Test
     void shouldAddInUsd() {
-        Money m  = new Money(3, USD);
-        assertThat(m.add(3)).isEqualTo(new Money(6, USD));
+        Money m  = new Money(3, USD); 
+        Money sum;
+
+        sum = assertDoesNotThrow(() -> m.add(new Money(3,USD)));
+        assertThat(sum).isEqualTo(new Money(6, USD));
     }
 
     @Test
@@ -22,5 +29,13 @@ class MoneyTest {
     void shouldDivideInKoreanWons() {
         Money m  = new Money(3, KRW);
         assertThat(m.divide(3)).isEqualTo(new Money(1, KRW));
+    }
+
+    @Test
+    void shouldNotAddInDifferentCurrency()
+    {
+        Money m  = new Money(3, USD);
+
+        assertThrows(Exception.class,() -> m.add(new Money(3,EUR)));
     }
 }
