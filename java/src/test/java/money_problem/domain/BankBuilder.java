ed9a1ebb56a55c948d68d25b58218390edb12756
mflorent <sandi.florent9@gmail.com>
2026-03-19 09:50:54 +0100

    chore(java) BankBuilder3 WIP

--- java/src/test/java/money_problem/domain/BankBuilder.java
+++ java/src/test/java/money_problem/domain/BankBuilder.java
@@ -11,13 +11,15 @@ public class BankBuilder {
 
     public BankBuilder withPivotCurrency(Currency currency) {
         this.currency = currency;
+        return this;
     }
 
     public BankBuilder withExchangeRate(Currency currency, double v) {
         this.exchangesRates.put(currency,v);
+        return this;
     }
 
     public Bank build() {
-        return Bank.withExchangeRate(currency, exchangesRates)
+        return Bank.withExchangeRate(currency, exchangesRates.keySet().toArray()[0]);
     }
 }
